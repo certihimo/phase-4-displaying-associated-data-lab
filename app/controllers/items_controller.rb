f92719cc68c0ac155dd32717c92175dccb3548ec
certihimo <kalidahm3d@gmail.com>
2023-06-22 11:31:44 +0300

    done

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,2 +1,18 @@
 class ItemsController < ApplicationController
+
+    def index 
+        items = Item.all 
+        item_data = items.map do |item|
+          {
+            id: item.id,
+            name: item.name,
+            description: item.description,
+            price: item.price,
+            user: item.user
+          }
+        end
+      
+        render json: item_data
+       end
+      
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,15 @@
 class UsersController < ApplicationController
+    
+    def show 
+        id = params[:id]
+        user = User.find(id)
+        item = item.where("user_id = ?", id )
+
+        render json: {
+            id: user.id,
+            username: user.username,
+            city: user.city,
+            items: item,
+        }
+    end    
 end
